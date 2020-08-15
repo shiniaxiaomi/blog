8733be68530168ec35324fdc9f73c158f9ccfd32
Yingjie.Lu <yingjie.lu@geely.com>
2020-08-15 16:51:17 +0800

    添加redis缓存配置

--- src/main/java/com/lyj/blog/config/RedisConfig.java
+++ src/main/java/com/lyj/blog/config/RedisConfig.java
@@ -1,25 +1,19 @@
 package com.lyj.blog.config;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.annotation.PropertyAccessor;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.cache.CacheKeyPrefix;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.cache.RedisCacheWriter;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.*;
-import org.springframework.format.support.DefaultFormattingConversionService;
 
 import javax.validation.constraints.NotNull;
 import java.time.Duration;
 import java.util.HashMap;
-import java.util.Map;
 
 /**
  * @author Yingjie.Lu
