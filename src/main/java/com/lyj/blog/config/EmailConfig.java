c32a796ef91699cad1e3bbb9ded291fb3b718c9b
Yingjie.Lu <yingjie.lu@geely.com>
2020-08-25 10:03:09 +0800

    解决邮箱发送到25端口超时问题，更改为465端口

--- src/main/java/com/lyj/blog/config/EmailConfig.java
+++ src/main/java/com/lyj/blog/config/EmailConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.DependsOn;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
+import java.util.Properties;
+
 /**
  * @author Yingjie.Lu
  * @description
@@ -17,12 +19,23 @@ public class EmailConfig {
     @DependsOn("loadConfig") //依赖于加载配置类，所以加载配置类会先加载
     @Bean
     public JavaMailSender javaMailSender(){
+        int port = 465;
+
         JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
         javaMailSender.setHost("smtp.qq.com");
-        javaMailSender.setPort(465);//设置端口
         javaMailSender.setUsername(LoadConfig.getInstance().getEmail());
         javaMailSender.setPassword(LoadConfig.getInstance().getEmailPassword());
         javaMailSender.setDefaultEncoding("utf-8");
+
+        // 配置端口和ssl（使用在阿里云和本地环境）
+        Properties properties = new Properties();
+        properties.setProperty("mail.smtp.auth", "true");//开启认证
+        properties.setProperty("mail.smtp.timeout", "1000");//设置链接超时
+        properties.setProperty("mail.smtp.port", Integer.toString(port));//设置端口
+        properties.setProperty("mail.smtp.socketFactory.port", Integer.toString(port));//设置ssl端口
+        properties.setProperty("mail.smtp.socketFactory.fallback", "false");
+        properties.setProperty("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+        javaMailSender.setJavaMailProperties(properties);
         return javaMailSender;
     }
 
