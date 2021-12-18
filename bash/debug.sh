ac22ecfeb63cbb0ad38c5fc94d396f7a5cc0d913
lyj283982 <lyj283982@alibaba-inc.com>
2021-12-18 19:51:40 +0800

    提交远程debug脚本

---
+++ bash/debug.sh
@@ -0,0 +1,17 @@
+# 关闭应用
+#应用名称
+appName="blog"
+app=$(jps | grep $appName | awk '{print $1}')
+if test ${app}null != "null"
+then
+    kill -9 $app
+fi
+
+# 启动应用
+projectPath=/root/code/$appName
+cd $projectPath/target
+name=$(ls |grep jar$)
+nohup java -Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=10000,suspend=n -jar \
+           -Dspring.profiles.active=prod -Dserver.port=80 $projectPath/target/$name >$projectPath/out &
+tail -f $projectPath/out
+
---
+++ debug.sh
@@ -0,0 +1,8 @@
+appName="blog"
+projectPath=/root/code/$appName
+
+bash $projectPath/bash/pull.sh
+
+bash $projectPath/bash/build.sh
+
+bash $projectPath/bash/debug.sh
